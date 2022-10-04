9f5202f508aafc59b461ac3af7e1321382a76239
shibadisaster <joaquin.crisologo@obf.ateneo.edu>
2022-10-04 18:56:22 +0800

    Add GuitarString functionality

--- guitarstring.py
+++ guitarstring.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
+from ringbuffer import *
+import math #idk if this is allowed
+import random #idk if this is allowed
+
+
+ENERGY_DECAY_FACTOR = 0.996 #temporary, remove whenever this isnt needed anymore
+
+
 class GuitarString:
     def __init__(self, frequency: float):
         '''
         Create a guitar string of the given frequency, using a sampling rate of 24000 Hz
         '''
-        # TO-DO: implement this
-        self.capacity = # TO-DO: the max capacity of the ring buffer based on the frequency
-        self.buffer =   # TO-DO: construct the ring buffer object
+        self.capacity = math.ceil(24000 / frequency)
+        self.buffer = RingBuffer(self.capacity)
+        self.ticks = 0
+
+        for _ in range(self.capacity):
+            self.buffer.enqueue(0)
+
 
     @classmethod
     def make_from_array(cls, init: list[int]):
@@ -23,26 +35,38 @@ class GuitarString:
             stg.buffer.enqueue(x)
         return stg
 
+
     def pluck(self):
         '''
         Set the buffer to white noise
         '''
-        # TO-DO: implement this
+        for _ in range(self.capacity):
+            self.buffer.dequeue()
+            self.buffer.enqueue( random.uniform(-0.5, 0.5) )
+
 
     def tick(self):
         '''
         Advance the simulation one time step by applying the Karplus--Strong update
         '''
-        # TO-DO: implement this
+
+        self.ticks += 1
+        
+        f1 = self.buffer.dequeue()
+        f2 = self.buffer.peek()
+
+        self.buffer.enqueue( (f1 + f2) / 2 * ENERGY_DECAY_FACTOR )
+        
 
     def sample(self) -> float:
         '''
         Return the current sample
         '''
-        # TO-DO: implement this
+        return self.buffer.peek()
+
 
     def time(self) -> int:
         '''
         Return the number of ticks so far
         '''
-        # TO-DO: implement this
+        return self.ticks
