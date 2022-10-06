295b8368640661e8b34556b3e4c2343c2fa598bb
ZuyiI <100923220+ZuyiI@users.noreply.github.com>
2022-10-06 14:43:46 +0800

    Update guitar.py

--- guitar.py
+++ guitar.py
@@ -10,12 +10,9 @@ if __name__ == '__main__':
 
 
     class GuitarStrings:
-        def __init__(self, y: int):
+        def __init__(self, y):
             self.y = y
-
-        def freq(self):
-            return GuitarString(440 * 1.059463 ** (self.y - 12))
-            
+            self.freq = GuitarString(440 * (1.059463 ** (self.y - 12)))
 
     keyList = []
     keyboard = "q2we4r5ty7u8i9op"
@@ -24,6 +21,7 @@ if __name__ == '__main__':
         keyList.append(GuitarStrings(i))
 
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -31,15 +29,13 @@ if __name__ == '__main__':
             elif event.type == pygame.KEYDOWN:
                 key = event.unicode
                 if key in keyboard:
-                    keyList[keyboard.index(key)].freq().pluck()
-
-                    print(keyboard.index(key))                    # to check if the key pressed corresponds to the correct index
-                    print(keyList[keyboard.index(key)].freq())    # to check if the freq() method works
+                    keyList[keyboard.index(key)].freq.pluck()
 
         sample = 0
         for s in keyList:
-            sample += s.freq().sample()
+            sample += s.freq.sample()
         play_sample(sample)
 
         for x in keyList:
-            x.freq().tick()
+            x.freq.tick()
+
